0cc22ea6fc046b799bb4f0ea181ee9b4b9d4da0a
zeoprogram <alzi014299@gmail.com>
2026-09-09 15:58:29 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
+from pydantic import BaseModel
+
+from routers.bookings import router as bookings_router
+class RootResponse(BaseModel):
+    message: str
+@api_router.get("/", response_model=RootResponse)
+async def root():
+    return RootResponse(message="Haryadi Garage API siap digunakan")
+
+
 @api_router.get("/meta")
 async def api_meta():
     return {"today": "", "workshop_name": "Haryadi Garage"}
 
 
-api_router.include_router(bookings_router
+api_router.include_router(bookings_router)
