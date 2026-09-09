eaa85e48dd62bc1d1003c5f0432f2ceb74d7f629
zeoprogram <alzi014299@gmail.com>
2026-09-09 16:06:23 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,7 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import Admin from "@/pages/Admin";
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
